df09387521fd593f89bf94312d9a1d8f13c9b4cd
Md Shahnazul Islam <92403203+shahnazulislam@users.noreply.github.com>
2022-10-19 17:01:37 +0600

    add a login file

--- users_account/index.php
+++ users_account/index.php
@@ -6,10 +6,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sign Up page design</title>
-  <?php
-  include 'css/style.php';
-  include 'link/link.php'
-  ?>
 
   <!-- font awesome cdn link -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -21,21 +17,27 @@
 
 <?php
 
-include 'dbcon.php';
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'useraccount';
+
+$con = mysqli_connect($server, $username, $password, $db);
 
 
 if(isset($_POST['submit'])){
-  $firstname =  $mysqli_real_escape_string($con, $_POST['firstname']);
-  $lastname = $mysqli_real_escape_string($con, $_POST['lastname']);
-  $email =  $mysqli_real_escape_string($con, $_POST['email']);
-  $phone =  $mysqli_real_escape_string($con, $_POST['phone']);
-  $password = $mysqli_real_escape_string($con, $_POST['password']);
-  $re_password =  $mysqli_real_escape_string($con, $_POST['re_password']);
+  $firstname =  $_POST['firstname'];
+  $lastname = $_POST['lastname'];
+  $email =  $_POST['email'];
+  $phone =   $_POST['contuct'];
+  $password = $_POST['password'];
+  $re_password =   $_POST['re_password'];
 
-  $pass = password_hash($password, PASSWORD_BCRYPT);
-  $re_pass = password_hash($re_password, PASSWORD_BCRYPT);
+  $hpassword = password_hash($password, PASSWORD_BCRYPT);
+  $hre_password = password_hash($password, PASSWORD_BCRYPT);
+ 
 
-  $emailquery = "select * form registration where email='$email' ";
+  $emailquery = "select * form users where email='$email' ";
   $query = mysqli_query($con, $emailquery);
   $emailcount = mysqli_num_rows($query);
 
@@ -44,10 +46,10 @@ if(isset($_POST['submit'])){
 
   }else{
     if($password === $re_password){
-      $insertquery = "INSERT INTO users( firstname, lastname, contuct, email, password, re_password) VALUES ('$firstname','$lastname','$phone','$email','$pass','$re_pass')";
+      $insertquery = "INSERT INTO users(`firstname`, `lastname`, `contuct`, `email`, `password`, `re_password`) VALUES ('$firstname','$lastname', '$phone','$email','$hpassword','$hre_password')";
 
       $iquery = mysqli_query($con, $insertquery);
-
+      echo $iquery;
       if($iquery){
         ?>
         <script>
@@ -65,7 +67,7 @@ if(isset($_POST['submit'])){
     }else{
       echo 'password not matching';
   }
-  }
+}
 
 }
 ?>
@@ -90,7 +92,7 @@ if(isset($_POST['submit'])){
       </div>
       <div class="input-group-prepant d-flex my-3">
         <span class="input-group-text"><i class="fa fa-phone"></i></span>
-        <input class="form-control" type="number" name="phone" placeholder="Phone number">
+        <input class="form-control" type="number" name="contuct" placeholder="Phone number">
       </div>
       <div class="input-group-prepant d-flex my-3">
         <span class="input-group-text"><i class="fa fa-lock"></i></span>
@@ -100,10 +102,22 @@ if(isset($_POST['submit'])){
         <span class="input-group-text"><i class="fa fa-lock"></i></span>
         <input class="form-control" type="password" name="re_password" placeholder="Re-Password">
       </div>
-      <submit class="btn btn-primary d-block my-3" type="submit" neme="submit" href="#">Submit Now</submit>
+      <input type="submit" name="submit" id="submit" class="button btn-primary form-control mb-2" value="Submit"/>
     </form>
     <p class="an_account">Alrady have an account? <a href="#">Sign in</a></p>
   </div>
+
+
+  <!--=============================
+        script include here start 
+    ==============================-->
+  <!-- jquery script include here -->
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+  <!-- bootstrap min.js include -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <!--==============================
+        script include end here
+  ===============================-->
 </body>
 
 </html>
---
+++ users_account/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login into php</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <h1 class="text-primary">Input the currect value in this fild</h1>
+    <div>
+        <span class="input-group-text"><i class="fa fa-user"></i></span>
+        <input type="text/email" name="username">
+    </div>
+    <div>
+        <span class="input-group-text"><i class="fa fa-user"></i></span>
+        <input type="password" name="password">
+    </div>
+    
+
+
+<!-- jquery cdn -->
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<!-- bootstrap cdn -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
