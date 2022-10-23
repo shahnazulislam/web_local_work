be0eca8bf5478da21f4ded5882bfea8922d975c0
Md Shahnazul Islam <92403203+shahnazulislam@users.noreply.github.com>
2022-10-23 18:15:12 +0600

    'update'

---
+++ w3schools/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+/*$name = array("Shakil", "Sumon", "Shariful");
+echo "I Like ".$name[0].", ".$name[1]." and ".$name[2].".";
+if($name){
+?>
+<script>
+    alert('data insart successfully')
+</script>
+<?php
+}else{
+    ?>
+    <script>
+        alert('data not insart successfully')
+    </script>
+    <?php
+}
+echo "<hr/>";
+
+$x = 5;
+$y = 10;
+function myTest(){
+    global $x, $y;
+    $y = $x + $y;
+}
+
+myTest();
+echo $y;
+*/
+
+    
+/*    function myTest() {
+        
+        echo "<p>variable x outside function is: $x</p>";
+    }
+    class Car{
+        public $color;
+        public $model;
+        public function __construct($color, $model){
+            $this->color = $color;
+            $this->model = $model;
+        }
+        public function message(){
+            return "My favorit is ".$this->color." ".$this->model. "!";
+        }
+    }
+
+    $myCar = new Car("black", "volvo");
+    echo $myCar-> message();
+    echo "<br/>";
+    $myMother = new Car("Most.", "Shamsunnahar Begom");
+    echo $myMother-> message();
+    echo "<br/>";
+    $myFather = new Car("Md. Badiuszzaman", "mia");
+    echo $myFather-> message();*/
+
+ function
+
+
+
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
