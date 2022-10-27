6db35316cdde93ccb2e20906094efcfb37166e1f
Md Shahnazul Islam <92403203+shahnazulislam@users.noreply.github.com>
2022-10-27 10:53:17 +0600

    add some code

--- MySQLi_data/update.php
+++ MySQLi_data/update.php
@@ -1,36 +1,19 @@
-   <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>create</title>
-</head>
-
-<body>
-
-    <?php
+  <?php
     include 'header.php';
     include 'config.php';
     include 'database.php';
-
-    ?>
-
+  ?>
     
-    
-    
-    <?php
-	    $id = $_GET['id'];
-	    $db = new Database();
-	    $query = "SELECT * FROM tbl_user WHERE id = ";
-	    $myData = $db->select->query($query)->fatch_assoc();
+  <?php
+	  $id = $_GET['id'];
+	  $db = new Database();
+	  $query = "SELECT * FROM tbl_user WHERE id = $id";
+	  $myData = $db->select->query($query)->fatch_assoc();
 	?>
-
-
-    <?php
+  
+  <?php
     $db = new Database();
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['submit'])) {  
         $name    = mysqli_real_escape_string($db->link, $_POST['name']);
         $email   = mysqli_real_escape_string($db->link, $_POST['email']);
         $mySkill = mysqli_real_escape_string($db->link, $_POST['skill']);
@@ -41,39 +24,36 @@
             $create = $db->insert($query);
         }
     }
-    ?>
-    <?php
+  ?>
+  
+  <?php
     if (isset($error)) {
-        echo "<span style='color:red'>" . $error . "<span>";
+      echo "<span style='color:red'>" . $error . "<span>";
     }
-    ?>
+  ?>
     <div class="col-md-6 mx-auto">
     <form action="create.php" method="post">
-        <table>
-            <tr>
-                <td>Name</td>
-                <td><input class="form-control" type="text" name="name" value="<?php echo ?>"></td>
-            </tr>
-            <tr>
-                <td>Gmail</td>
-                <td><input class="form-control" type="text" name="email" placeholder="Please inter email"></td>
-            </tr>
-            <tr>
-                <td>Skill</td>
-                <td><input class="form-control" type="text" name="skill" placeholder="Please inter skill"></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>
-                    <input class="btn btn-primary" type="submit" name="submit" value="submit">
-                    <input class="btn btn-dark text-light" type="reset" value="Cancel">
-                </td>
-            </tr>
-        </table>
+      <table>
+        <tr>
+          <td>Name</td>
+          <td><input class="form-control" type="text" name="name" value="<?php echo $_GET['id'] ?>"></td>
+        </tr>
+        <tr>
+          <td>Gmail</td>
+          <td><input class="form-control" type="text" name="email" placeholder="Please inter email"></td>
+        </tr>
+        <tr>
+          <td>Skill</td>
+          <td><input class="form-control" type="text" name="skill" placeholder="Please inter skill"></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <input class="btn btn-primary" type="submit" name="submit" value="submit">
+            <input class="btn btn-dark text-light" type="reset" value="Cancel">
+          </td>
+        </tr>
+      </table>
     </form>
     <a class="text-decoration-none" href="index.php">Go Back</a>
     </div>
-    
-</body>
-
-</html>
