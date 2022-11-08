daad760d71eb54d9775b3bed82a83620a9a04297
shahnazulislam <mdshahnazulislam@gmail.com>
2022-11-08 17:57:41 +0600

    add one another code

--- row_php_crud_operation/loop.php
+++ row_php_crud_operation/loop.php
@@ -129,7 +129,27 @@ sum($one, $two)*/
 
 //function with returning value
 
+function sum($math, $eng, $sc){
+  $s = $math + $eng + $sc;
+
+  return $s;
+}
+function percentage($st){
+  $per = $st /3;
+
+  echo $per ."%";
+}
+
+$total = sum(55,65,88);
+
+percentage($total);
+
+/*echo $total;*/
+
+
+
 
 
 
 ?>
+
