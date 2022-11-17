5db2cead1d942132c02a7486f95532fa7c9af376
Killer <killer@kali.Killer>
2022-11-17 16:39:59 +0600

    add student sqli_query function

---
+++ sqli_work/sqli_fetch_function.php
@@ -0,0 +1,24 @@
+<?php 
+/*
+	mysqli_fetch_assoc();
+	mysqli_fetch_array();
+	mysqli_fetch_row();
+	mysqli_fetch_all();
+	mysqli_fetch_field();
+	*/
+
+	$conn 	= mysqli_connect("localhost", "root", "", "test") or die("Connection faild");
+	$sql  	= "SELECT * FROM student";
+	$result = mysqli_query($conn, $sql) or die("query faild");
+
+	$row = mysqli_fetch_assoc($result);
+
+	/*echo "<pre>";
+	print_r($row);
+	echo "</pre>";
+*/
+
+	echo $row['first_name']. " ". $row['last_name'];
+	
+
+ ?>
