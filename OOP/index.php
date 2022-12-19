07fe49e6f4f380eed054daed658182f51a9fd2d5
Md Shahnazul Islam <mdshahnazulislam@gmail.com>
2022-12-19 17:57:23 +0600

    add oop in php folder

---
+++ OOP/index.php
@@ -0,0 +1,30 @@
+<?php
+class calculation
+{
+    public $a, $b, $c;
+
+
+    function sum()
+    {
+        $this->c = $this->a + $this->b;
+        return $this->c;
+    }
+    function sub()
+    {
+        $this->c = $this->a - $this->b;
+        return $this->c;
+    }
+}
+$c1 = new calculation();
+$c1->a = 20;
+$c1->b = 10;
+
+$c2 = new calculation();
+$c2->a = 30;
+$c2->b = 10;
+
+echo "Sub value C1: " . $c2->sub() . "<br>";
+echo "Sum value C1: " . $c1->sum() . "<br>";
+echo "Sum value C1: " . $c2->sum();
+
+?>
